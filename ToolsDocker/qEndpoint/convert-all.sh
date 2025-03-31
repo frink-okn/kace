9521b28c27f05f1bdd390674f836eeaa50ece79f
YaphetKG <kebedey@renci.org>
2025-03-31 11:26:52 -0400

    pushing compression and ingress changes

--- ToolsDocker/qEndpoint/convert-all.sh
+++ ToolsDocker/qEndpoint/convert-all.sh
@@ -14,26 +14,58 @@ export RIOT_TMP_DIR=$WORKING_DIR/riot-tmp
 export HDT_TMP_DIR=$WORKING_DIR/hdt-tmp
 export HDT_FINAL_DIR=$WORKING_DIR/hdt
 export REPORT_DIR=$WORKING_DIR/report
+export JAVATMP_DIR=$WORKING_DIR/java-temp
 
 rm -rf $RIOT_TMP_DIR/*
 rm -rf $HDT_TMP_DIR/*
 rm -rf $HDT_FINAL_DIR/*
 rm -rf $REPORT_DIR/*
+rm -rf $JAVATMP_DIR/*
 
 mkdir -p ${RIOT_TMP_DIR}
+mkdir -p ${JAVATMP_DIR}
+
+export JAVA_OPTIONS="${JAVA_OPTIONS} -Djava.io.tmpdir=${JAVATMP_DIR}"
+echo $JAVA_OPTIONS
+# Build an array of files to process.
+# For compressed files (.gz or .bz2), uncompress them into RIOT_TMP_DIR and add the uncompressed file.
 FILES=()
 for file in "$@"; do
     file_full_path="${WORKING_DIR}/${file}"
-    FILES+=("$file_full_path")
+    if [[ "$file_full_path" == *.gz ]]; then
+        uncompressed_file="${RIOT_TMP_DIR}/$(basename "$file_full_path" .gz)"
+        echo "Uncompressing $file_full_path to $uncompressed_file"
+        gunzip -c "$file_full_path" > "$uncompressed_file"
+        FILES+=("$uncompressed_file")
+    elif [[ "$file_full_path" == *.bz2 ]]; then
+        uncompressed_file="${RIOT_TMP_DIR}/$(basename "$file_full_path" .bz2)"
+        echo "Uncompressing $file_full_path to $uncompressed_file"
+        bzip2 -dc "$file_full_path" > "$uncompressed_file"
+        FILES+=("$uncompressed_file")
+    else
+        FILES+=("$file_full_path")
+    fi
 done
 
-echo "combining "
-riot --merge --debug  --nocheck -v --output TURTLE ${FILES[@]} > ${RIOT_TMP_DIR}/combined.ttl
+echo "Combining files:"
+echo "${FILES[@]}"
+
+# Process files: if a single file is provided, convert it to Turtle (if needed)
+# for compatibility with rdf2hdt.sh; otherwise merge the files.
+if [ "${#FILES[@]}" -eq 1 ]; then
+    echo "Single file provided. Converting to Turtle format if necessary."
+    INPUT_FILE="${FILES[0]}"
+else
+    echo "Multiple files provided. Merging files:"
+    echo "${FILES[@]}"
+    riot --merge --debug  --nocheck -v --output NT  ${FILES[@]} > ${RIOT_TMP_DIR}/combined.nt
+    INPUT_FILE=${RIOT_TMP_DIR}/combined.nt
+fi
 
 echo "validating..."
 mkdir -p $REPORT_DIR
 set +e
-riot --validate --check --strict --sink ${RIOT_TMP_DIR}/combined.ttl > $REPORT_DIR/riot_validate.log 2>&1
+riot --validate --check --strict --sink ${INPUT_FILE} > $REPORT_DIR/riot_validate.log 2>&1
 set -e
 #python3 /bin/process_graph.py ${RIOT_TMP_DIR}/ $REPORT_DIR
 
@@ -42,9 +74,9 @@ mkdir -p ${HDT_FINAL_DIR}
 
 cd ${WORKING_DIR}
 
-rdf2hdt.sh -cattree -cattreelocation ${HDT_TMP_DIR} -index ${RIOT_TMP_DIR}/combined.ttl ${HDT_FINAL_DIR}/graph.hdt
+rdf2hdt.sh -cattree -cattreelocation ${HDT_TMP_DIR} -index ${INPUT_FILE} ${HDT_FINAL_DIR}/graph.hdt > /dev/null
 
 rm -rf ${RIOT_TMP_DIR}
 rm -rf ${HDT_TMP_DIR}
-
+rm -rf ${JAVATMP_DIR}
 
